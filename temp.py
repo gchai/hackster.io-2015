7a19e5550385b70644e00fba8b10de818d9038c4
spokenlore <spokenlore@gmail.com>
2015-05-23 15:48:42 -0400

    Added missing (

--- temp.py
+++ temp.py
@@ -8,6 +8,6 @@ tempio = mraa.Aio(0)
 
 while 1:
     tempres = float(1023-tempio * 10000/tempio)
-    tempC = 1/log(tempres/10000)/3975+1/298.15)-273.15
+    tempC = 1/(log(tempres/10000)/3975+1/298.15)-273.15
 	time.sleep(.2)
 	print tempC
