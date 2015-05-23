0a1b2d7b266859fc57ae980c42bad68020a350bb
spokenlore <spokenlore@gmail.com>
2015-05-23 15:50:40 -0400

    fixed errors

--- temp.py
+++ temp.py
@@ -7,7 +7,7 @@ import time
 tempio = mraa.Aio(0)   
 
 while 1:
-    tempres = float(1023-tempio * 10000/tempio)
+    tempres = float(1023-float(tempio) * 10000/float(tempio))
     tempC = 1/(log(tempres/10000)/3975+1/298.15)-273.15
-	time.sleep(.2)
+    time.sleep(.2)
 	print tempC
