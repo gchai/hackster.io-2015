66efea1897dea6ffbfe5252ba0c099b4eb45e1c9
gchai <g@bechai.com>
2015-05-23 23:34:19 +0000

    Fixed buttons.py

--- buttons.py
+++ buttons.py
@@ -9,8 +9,9 @@ buttonState = 0
 buttonPin = 2
 
 while 1:
+	time.sleep(.5)
 	button = mraa.Gpio(buttonPin)
 	button.dir(mraa.DIR_IN)
 	buttonState = button.read()
 	print buttonState
-	time.sleep(1)
+
