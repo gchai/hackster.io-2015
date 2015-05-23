105b53de62adccd2cd80638b6003d6e608100a58
spokenlore <spokenlore@gmail.com>
2015-05-23 16:31:01 -0400

    Added LCD Display for temp.py

--- temp2.py.py
+++ temp2.py.py
@@ -6,6 +6,7 @@ import time
 import socket
 import fcntl
 import struct
+import math
 
 import pyupm_12clcd as lcd
 
@@ -16,9 +17,12 @@ myLCD = lcd.Jhd1313ml(0, 0x3E, 0x62)
 
 while 1:
 	tempread = tempio.read()
-    tempres = float(1023-tempread)*10000/tempread
-    tempC = 1/(log(tempres/10000)/3975+1/298.15)-273.15
-    print tempC
+    tempres = float(1023-tempread) * 10000/tempread
+    tempC = 1/(match.log(tempres/10000)/39.75 + 1/298.15) - 273.15
+    tempF = tempC*9/5 + 32
+
+    print "TempC: %f, TempF: %f" % (tempC, tempF)
+    
     myLCD.clear()
     myLCD.setColor(255,255,0)
     myLCD.setCursor(0,0)
