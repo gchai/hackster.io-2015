27e03422ce545f745e2b702399c2a1e3d0e3de43
spokenlore <spokenlore@gmail.com>
2015-05-24 13:41:31 -0400

    Changes to main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# Sous Vide Machine w/ Arduino Controls
+# For Hackster.io 2015
+# May 24, 2015
+# Samson Fung, Gabriel Chai, Michael Sy, Zoilo Mercedes, Kevin Yan
+
 import mraa
-import sys
 import time
-import socket
-import fcntl
-import struct
 import math
 
 import pyupm_i2clcd as lcd
@@ -12,56 +13,38 @@ tempio = mraa.Aio(0)
 # Initialize Jhd1313m1 @ 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)   
 myLCD = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
-temperature = 100
-clocktime = 10
-
 tempUpPin = 2 
 tempDownPin = 4
 timeUpPin = 6
 timeDownPin = 8
+startButtonPin = 10
 
-tempUp = mraa.Gpio(tempUpPin)
-tempUp.dir(mraa.DIR_IN)
-
-tempDown = mraa.Gpio(tempDownPin)
-tempDown.dir(mraa.DIR_IN)
-
-timeUp = mraa.Gpio(timeUpPin)
-timeUp.dir(mraa.DIR_IN)
-
-timeDown = mraa.Gpio(timeDownPin)
-timeDown.dir(mraa.DIR_IN)
-
+# Code is for Fahrenheit temperatures
 def TempChange(tempUp, tempDown):
 	global temperature
 	if temperature >= 80:
-		if tempUp == 1:
+		if tempUp.read() == 1:
 			temperature += 1
 		if not(temperature == 80):
-			if tempDown == 1:
+			if tempDown.read() == 1:
 				temperature -= 1
 	return temperature
 
 def TimeChange(timeUp, timeDown):
 	global clocktime
 	if clocktime >= 0:
-		if timeUp == 1:
+		if timeUp.read() == 1:
 			clocktime += 10
 		if not(clocktime == 0):
-			if timeDown == 1:
+			if timeDown.read() == 1:
 				clocktime -= 10
 	return clocktime
 
+# For Analog Reader
+def temperature():
 
+	tempread = tempio.read()
 
-
-
-
-
-
-
-
-while 1:
 	time.sleep(.5)
 	
 	tempread = tempio.read()
@@ -79,9 +62,46 @@ while 1:
 
 	myLCD.write(bottomDisplay)
 
-
-
 	print "Temperature Up: %d, Temperature Down: %d" % (tempUp.read(), tempDown.read())
 	print "Time Up: %d, Time Down: %d" % (timeUp.read(), timeDown.read())
 
+	return (tempC, tempF)
 
+def display(line1, line2):
+	myLCD.clear()
+	myLCD.setColor(255,255,0)
+	myLCD.setCursor(0,0)
+	myLCD.write(str(line1))
+
+	myLCD.setCursor(1,0)
+	myLCD.write(str(line2))
+
+def main():
+	tempUp = mraa.Gpio(tempUpPin)
+	tempUp.dir(mraa.DIR_IN)
+
+	tempDown = mraa.Gpio(tempDownPin)
+	tempDown.dir(mraa.DIR_IN)
+
+	timeUp = mraa.Gpio(timeUpPin)
+	timeUp.dir(mraa.DIR_IN)
+
+	timeDown = mraa.Gpio(timeDownPin)
+	timeDown.dir(mraa.DIR_IN)
+
+	startButton = mraa.Gpio(startButtonPin)
+	startButton.dir(mraa.DIR_IN)
+
+	initTemp = 100
+	initTime = 10
+
+	while start == False:
+		newTemp = TempChange(tempUp, tempDown)
+		newTime = TimeChange(timeUp, timeDown)
+
+		line1 = "Temperature is: " + newTemp + "degrees F."
+		line2 = "Timer: " + newTime + " minutes"
+		display(newTemp, newTime)
+		if startButton.read() == 1:
+			start = True
+	display(newTemp, newTime)
